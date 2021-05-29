ab54c2fdbe427cb67a324a4d4b4056c8d074e6ad
rubiocode <78938193+rubiocode@users.noreply.github.com>
2021-05-29 00:58:53 -0700

    fixed local storage in JS, added HTML buttons, added CSS to HTML

--- Assets/main.js
+++ Assets/main.js
@@ -1,25 +1,42 @@
 $(document).ready(function (){
+    // getting prev search history from local storage upon page loading
+
+    function cityAllStorage (){
+        let cities = [];
+        keys = Object.keys(localStorage);
+        i= keys.length;
+        while (i--){
+            cities.push(JSON.parse(localStorage[keys[i]]));
+        }
+        for (j=0; j<cities.length; j++) {
+            $(".city-list").prepend("<button type='button' class='btn btn-light prev-city'>"+ cities[j]+ "</button>");
+        }
+        
+    }
+    cityAllStorage();
+
+    // removing stored cities upon click event
+
+    $(".remove").on("click", function (){
+        localStorage.clear();
+        $(".prev-city").remove();
+    });
 
     //getting time on header set up using moments.js
     let today = function timeStamp (){
         $('#currentDay').text(`${moment().format('MMMM Do YYYY, h:mm a')}`);
         };
         today();
-        //setInterval function to update every second so the today function keeps updating
+    //setInterval function to update every second so the today function keeps updating
         setInterval(today, 1000);
 
 
-        renderCityList();
-        console.log(renderCityList)
-
     // obtain current weather upon city search, setting up local storage
-    let cityInput= document.querySelector('#city-list');
-   let cities= [];
-     $(".myClass").on('click', function (event){
+    $(".myClass").on('click', function (event){
         event.preventDefault();
 
         let city= $("#search").val().trim();
-        cities = city.split("")
+        cities = city.split(",")
         if (city != ''){
             $.ajax({
                 url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=50f9be4cd5ddca3502184f3307bce83e`,
@@ -33,9 +50,9 @@ $(document).ready(function (){
                 }  
             })
             localStorage.setItem("cities", JSON.stringify(cities));
-            renderCityList();
+            $(".city-list").prepend("<button class='btn btn-light prev-city'>"+ cities+ "</button>")
             fiveDayForecast (city);
-        }else {
+        }else{
             $('#error').html (alert('Field cannot be empty'))
         }
 
@@ -67,7 +84,6 @@ $(document).ready(function (){
                     
                     if(data.list[i].dt_txt.indexOf("15:00:00")>0){
                         let weatherData= data.list[i]
-                        console.log(weatherData)
                         let widgetFiveDay = showForecastData(weatherData);
 
                         $("#showFiveDayForecast").append(widgetFiveDay);
@@ -76,8 +92,11 @@ $(document).ready(function (){
                 
             }  
     })
+
     function showForecastData (data){
-        return  "<div>" + "<h3><img src=http://openweathermap.org/img/wn/"+ data.weather[0].icon+".png id='img2'> "+ data.weather[0].description  +"</h3>" + 
+        
+        return  "<div> <h3 class='dateFive'></h3>" +
+                "<h3><img src=http://openweathermap.org/img/wn/"+ data.weather[0].icon+".png id='img2'> "+ data.weather[0].description  +"</h3>" + 
                 "<h3><strong>Weather</strong>: "+ data.weather[0].main  +"</h3>" +
                 "<h3><strong>Temperature</strong>: "+ Math.floor(data.main.temp)  +"&deg;F</h3>" +
                 "<h3><strong>Wind Speed</strong>: "+ Math.floor(data.wind.speed)  +" MPH" +"</h3>" +
@@ -85,36 +104,15 @@ $(document).ready(function (){
         };
 
     }
-
-// render city list
     
-    function renderCityList () {
-        cityInput.innerHTML="";
-            for (i=0; i < cities.length; i++){
-                let city = cities[i];
+$(document).on("click",".prev-city", function(){
+    let city = $(this).text();
+    $(".inpSearch").val(city);
+    $("#searchButton").click();
+    $(this).remove();
 
-                let li= document.createElement("li");
 
-                li.textContent= city;
-                li.setAttribute("data-index", i);
-
-                let button = document.createElement("button");
-                button.textContent= "Remove";
-
-                li.appendChild(button);
-                cityInput.appendChild(li);
-            }
-        }
-
-    function init (){
-        let cityList= JSON.parse(localStorage.getItem("cities"));
-
-        if (cityList !== null) {
-            cities = cityList;
-        }
-        renderCityList();
-    }
-    init();
+});
 
 })
 
